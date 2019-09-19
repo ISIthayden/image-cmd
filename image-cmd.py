a220e5c789498646d7cc100f86a856bddf29d0d3
Tony Hayden <proximality24@gmail.com>
2019-09-19 12:18:14 -0700

    fixed the deletion line...

--- image-cmd.py
+++ image-cmd.py
@@ -12,15 +12,15 @@ import random
 import subprocess
 
 # request url to scrape from user and assign it to a varible
-url = input("Please enter a website to obtain images from:\n")
+# url = input("Please enter a website to obtain images from:\n")
 
 # call image-scraper and pass it the url, saving images in a new directory
-subprocess.call(['/usr/bin/image-scraper','-s images','-m 10',url])
+# subprocess.call(['/usr/bin/image-scraper','-s images','-m 10',url])
 
 # randomly select a file within the images directory and assign its path to a variable
-filePath = '/home/tony/image-cmd/ images/' + (random.choice(os.listdir('/home/tony/image-cmd/ images')))
+# filePath = '/home/tony/image-cmd/ images/' + (random.choice(os.listdir('/home/tony/image-cmd/ images')))
 
 # draw the image to the command line, using viu, passing it the path of the selected image
-subprocess.call(['/usr/bin/viu', filePath])
+# subprocess.call(['/usr/bin/viu', filePath])
 
-shutil.rmtree(shutil.rmtree("images", ignore_errors=True, onerror=None))
+shutil.rmtree("images", ignore_errors=True, onerror=None)
